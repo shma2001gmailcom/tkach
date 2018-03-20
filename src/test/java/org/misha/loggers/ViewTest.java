358bf8834c451b1f8a9649077d1242e737d2e1ae
misha <mashevelin.sbt@sberbank.ru>
2018-03-20 12:49:02 +0600

    remove redundant code
    (cherry picked from commit 6bda3a4)

--- src/test/java/org/misha/loggers/ViewTest.java
+++ src/test/java/org/misha/loggers/ViewTest.java
@@ -2,12 +2,14 @@ package org.misha.loggers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.misha.client.Kind;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import static org.junit.Assert.assertNotNull;
-import static org.misha.client.LoggingClient.Kind;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * author: misha
@@ -17,11 +19,27 @@ import static org.misha.client.LoggingClient.Kind;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
 @ContextConfiguration("file:src/main/webapp/WEB-INF/application-context.xml")
-
 public class ViewTest {
+    private static final AtomicInteger errorCount = new AtomicInteger(0);
     
     @Test
-    public void testView() throws Exception {
-        for (Kind kind : Kind.values()) assertNotNull(kind.getView());
+    public void testView() {
+        for (int i = 0; i < 1000; doTest(), ++i) ;
+    }
+    
+    private void doTest() {
+        int i = 0;
+        while (i < Kind.values().length) {
+            final int j = i;
+            new Thread(new Runnable() {
+                
+                @Override
+                public void run() {
+                    if (Kind.values()[j].getView() == null) errorCount.incrementAndGet();
+                }
+            }).start();
+            ++i;
+        }
+        assertTrue(errorCount.get() == 0);
     }
 }
