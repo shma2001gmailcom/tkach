de6f6523fd7d1ad852952f6f44a89b2cd2a89626
misha <mashevelin.sbt@sberbank.ru>
2018-03-21 16:47:29 +0600

    fix test

--- src/main/java/org/misha/client/Kind.java
+++ src/main/java/org/misha/client/Kind.java
@@ -7,19 +7,19 @@ import org.springframework.web.client.RestTemplate;
  * date: 3/11/18
  * time: 1:09 PM
  */
-
 public enum Kind {
     LIST("list"), CACHED("cached"), COMBINED("combined"), CONSOLE("console"), FILE("file"), DB("db");
     
     //string-value for kind
     private final String name;
+    private static final RestTemplate restTemplate = new RestTemplate();
     
     Kind(String kind) {
         name = kind;
     }
     
     private static String getLogs(final String kind) {
-        return new RestTemplate().getForObject("http://localhost:8080/tkach/rest/" + kind, String.class);
+        return restTemplate.getForObject("http://localhost:8080/tkach/rest/" + kind, String.class);
     }
     
     public String getView() {
--- src/test/java/org/misha/loggers/ViewTest.java
+++ src/test/java/org/misha/loggers/ViewTest.java
@@ -1,6 +1,6 @@
 package org.misha.loggers;
 
-import org.junit.Ignore;
+import org.apache.commons.lang3.concurrent.ConcurrentException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.misha.client.Kind;
@@ -8,8 +8,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static java.util.concurrent.Executors.newFixedThreadPool;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -17,25 +24,63 @@ import static org.junit.Assert.assertTrue;
  * date: 3/11/18
  * time: 2:48 PM
  */
-@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
 @ContextConfiguration("file:src/main/webapp/WEB-INF/application-context.xml")
 public class ViewTest {
     private static final AtomicInteger errorCount = new AtomicInteger(0);
+    private final Map<Kind, Callable<String>> map = new HashMap<>();
+    private final ExecutorService service = newFixedThreadPool(20);
     
-    @Test
-    public void testView() {
-        for (int i = 0; i < 3; ++i) {
-            assertRendered();
+    {
+        for (final Kind kind : Kind.values()) {
+            map.put(kind, new CallableView(kind));
         }
     }
     
-    private void assertRendered() {
+    private static void assertRendered() {
         for (Kind kind : Kind.values()) {
-            final String view = kind.getView();
+            String view = kind.getView();
             if (view == null) errorCount.incrementAndGet();
         }
         assertTrue(errorCount.get() == 0);
     }
+    
+    @Test
+    public void testView() throws ConcurrentException, ExecutionException, InterruptedException {
+        for (int i = 0; i < 10; ++i) {
+            for (final Kind kind : Kind.values()) {
+                assertNotNull(service.submit(map.get(kind)).get());
+            }
+        }
+        service.shutdown();
+        assertRendered();
+    }
+    
+    @Test
+    public void testViewAnotherWay() {
+        for (int i = 0; i < 10; ++i) {
+            new Thread(new Runnable() {
+                
+                @Override
+                public void run() {
+                    assertRendered();
+                }
+            }).start();
+        }
+    }
+    
+    private static class CallableView implements Callable<String> {
+        private final Kind kind;
+        
+        private CallableView(final Kind kind) {
+            this.kind = kind;
+        }
+        
+        @Override
+        public String call() throws Exception {
+            return kind.getView();
+        }
+    }
 }
+
